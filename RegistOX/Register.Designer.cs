290e8ba27eb0096e953a15eb4653965e6ecc7446
qqexwed3 <59160935@go.buu.ac.th>
2018-09-12 01:03:46 +0700

    Register V1

---
+++ RegistOX/Register.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace RegistOX
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(87, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Register";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightYellow;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(41, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Username";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightYellow;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.Location = new System.Drawing.Point(41, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 30);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Password";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LightYellow;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox3.Location = new System.Drawing.Point(41, 198);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(207, 30);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Name";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // OK
+            // 
+            this.OK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.OK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.OK.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(5)))));
+            this.OK.Location = new System.Drawing.Point(41, 262);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 37);
+            this.OK.TabIndex = 4;
+            this.OK.Text = "Register";
+            this.OK.UseVisualStyleBackColor = false;
+            this.OK.Click += new System.EventHandler(this.OK_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(173, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, -2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(10, 10);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Register
+            // 
+            this.BackgroundImage = global::RegistOX.Properties.Resources.modern_technological_elements_with_transparent_background_1035_7108backg;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Name = "Register";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Register Now!";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        /*private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userRegister;
+        private System.Windows.Forms.TextBox userNameRegister;
+        private System.Windows.Forms.TextBox passwordRegister;
+        private System.Windows.Forms.Button signIn;
+        private System.Windows.Forms.Button back;*/
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ RegistOX/Register.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+
+namespace RegistOX
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Black;
+            textBox1.Text = "";
+        }
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.ForeColor = Color.Black;
+            textBox2.Text = "";
+            textBox2.PasswordChar = '*';
+        }
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.ForeColor = Color.Black;
+            textBox3.Text = "";
+        }
+        class User
+        {
+            public ObjectId _id { get; set; }
+
+            public string Username
+            {
+                get; set;
+            }
+            public string Password
+            {
+                get; set;
+            }
+            public string Name
+            {
+                get; set;
+            }
+            public string Avatar
+            {
+                get; set;
+            }
+            public int Win
+            {
+                get; set;
+            }
+            public int Draw
+            {
+                get; set;
+            }
+            public int Lose
+            {
+                get; set;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OK_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                MongoClient client = new MongoClient("mongodb://admin:a123456@ds141902.mlab.com:41902/ox");
+                MongoServer server = client.GetServer();
+                MongoDatabase database = server.GetDatabase("ox");
+                MongoCollection mongoCollection = database.GetCollection<User>("User");
+                User user = new User();
+                BindingList<User> doclist = new BindingList<User>();
+                var getCollection = database.GetCollection<User>("User");
+                var insertUser = getCollection.AsQueryable().Where(pd => pd.Username == textBox1.Text);
+
+                foreach (var p in insertUser)
+                {
+                    doclist.Add(p);
+                    Application.DoEvents();
+                }
+                dataGridView1.DataSource = doclist;
+                if (dataGridView1.Rows.Count == 0)
+                {
+                    if (!string.IsNullOrEmpty(textBox1.Text.Trim()) && !textBox1.Text.Equals("Username"))
+                    {
+                        user.Username = textBox1.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณากรอกUsername");
+                    }
+                    if (!string.IsNullOrEmpty(textBox2.Text.Trim()) && !textBox2.Text.Equals("Password"))
+                    {
+                        user.Password = textBox2.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณากรอกPassword");
+                    }
+                    if (!string.IsNullOrEmpty(textBox3.Text.Trim()) && !textBox3.Text.Equals("Name"))
+                    {
+                        user.Name = textBox3.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณากรอกName");
+                    }
+                    user.Avatar = null;
+                    user.Win = 0;
+                    user.Draw = 0;
+                    user.Lose = 0;
+
+                    if (!string.IsNullOrEmpty(textBox1.Text.Trim()) &&
+                        !string.IsNullOrEmpty(textBox2.Text.Trim()) &&
+                        !string.IsNullOrEmpty(textBox3.Text.Trim()) &&
+                        (!textBox1.Text.Equals("Username") &&
+                         !textBox2.Text.Equals("Password") &&
+                         !textBox3.Text.Equals("Name")))
+                    {
+                        mongoCollection.Insert(user);
+                        MessageBox.Show("สมัครเสร็จสิ้น");
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex);
+            }
+        }
+    }
+}
